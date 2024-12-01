9bfd3e2389269e2a13e9df3cf18e6681b277e39e
Abel Angel Ortega <u20201b380@upc.edu.pe>
2024-12-01 14:01:22 -0500

    Initial Commit

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,13 +5,95 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+M.ui = {
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default",
+    border_color = "nord_blue",
+    selected_item_bg = "colored"
+  },
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+  },
+  statusline = {
+    theme = "vscode_colored"
+  },
+  telescope = {
+    style = "borderless"
+  },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+   " ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+   " ⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣿⣿⣿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀",
+   " ⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀",
+   " ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀",
+   " ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡅⠀⣸⣿⣿⣿⠿⠟⠛⠿⣿⠆⠀⠀⠀⠀",
+   " ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⠟⠁⣠⣶⣶⣦⣤⣤⣾⣷⣄⠀",
+   " ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠛⠉⣁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇",
+   " ⠀⠀⠀⠀⠀⣀⠀⠀⠀⣴⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀",
+   " ⢠⣤⣄⡀⣼⣿⣧⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀",
+   " ⠘⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣩⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀",
+   " ⠀⠈⠻⢿⣿⣿⣿⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀",
+   " ⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀",
+   " ⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠿⠿⠿⠿⠟⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+   " ⠀⠀⠀ ⠀                ⠀  ⠀⠀⠀⠀",
+   " ⠀⠀⠀ ⠀SWIMMING IN CODE⠀  ⠀⠀⠀⠀",
+   " ⠀⠀⠀ ⠀                ⠀  ⠀⠀⠀⠀",
+   " ⠀⠀⠀ ⠀                ⠀  ⠀⠀⠀⠀",
+  },
+}
+
+M.base46 = {
+	theme = "solarized_dark",
+  transparency = true,
+
+  hl_add = {
+    NvimTreeOpenedFolderName = {fg = "blue", bold = true},
+  },
+
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+    CursorLine = {
+      bg = "one_bg"
+    },
+    Visual = {
+      bg = "one_bg3"
+    },
+    NvDashAscii = {
+      bg = "NONE",
+      fg = "white"
+    },
+    NvimTreeFolderArrowClosed = {
+      fg = "nord_blue",
+    },
+    NvimTreeFolderIcon = {
+      fg = "nord_blue",
+    },
+    NvimTreeFolderName = {
+      fg = "nord_blue"
+    },
+    NvimTreeOpenedFolderName = {
+      fg = "white"
+    },
+    NvimTreeFolderArrowOpen = {
+      fg = "white",
+      underdashed = true
+    },
+    NvimTreeCursorLine = {
+      bg = "one_bg"
+    },
+    NvDashButtons = {
+      bg = "NONE",
+      fg = "white"
+    },
+	},
 }
 
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,10 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.ruler = true
+o.relativenumber = true
+o.number = true
+o.cursorlineopt = "number,line"
+o.termguicolors = true
 -- o.cursorlineopt ='both' -- to enable cursorline!
